16025ca39a0acb86ef31cb713fb9143c4b136e28
Abhishek7204 <abhishek021204@gmail.com>
2025-10-08 23:37:53 +0530

    Added sphere UV mapping

--- sphere.cpp
+++ sphere.cpp
@@ -1,7 +1,23 @@
 #include "sphere.h"
 #include "interval.h"
 #include "ray.h"
+#include <cmath>
 
+sphere::sphere(const point3 &center, double radius, shared_ptr<material> mat)
+    : centerLocations(center, vect(0, 0, 0)), radius(max(0.0, radius)),
+      mat(mat) {
+  vect rVec = vect(1, 1, 1) * radius;
+  bbox = aabb(center - rVec, center + rVec);
+}
+sphere::sphere(const point3 &center1, const point3 &center2, double radius,
+               shared_ptr<material> mat)
+    : centerLocations(center1, center2 - center1), radius(max(0.0, radius)),
+      mat(mat) {
+  vect rVec = vect(1, 1, 1) * radius;
+  aabb box1 = aabb(center1 - rVec, center1 + rVec);
+  aabb box2 = aabb(center2 - rVec, center2 + rVec);
+  bbox = aabb(box1, box2);
+}
 bool sphere::isHit(const ray &r, interval ray_t, hitRecord &record) const {
   // (C - P).(C - P) = (Cx - x)^2 + (Cy - y)^2 + (Cz - z)^2 = r^2
   // (C - (Q + dt)).(C - (Q + dt)) = r^2
@@ -28,5 +44,14 @@ bool sphere::isHit(const ray &r, interval ray_t, hitRecord &record) const {
   record.contactPoint = r.at(root);
   record.hitNormal = (record.contactPoint - center) / radius;
   record.hitMaterial = mat;
+  getUV(record.contactPoint, record);
   return true;
+};
+
+void sphere::getUV(const point3 &p, hitRecord &record) const {
+  double theta = acos(-p.y());
+  double phi = atan2(-p.z(), p.x()) + pi;
+
+  record.u = phi / (2 * pi);
+  record.v = theta / pi;
 };
--- sphere.h
+++ sphere.h
@@ -13,26 +13,16 @@ class sphere : public sceneObject {
   aabb bbox;
 
 public:
-  sphere(const point3 &center, double radius, shared_ptr<material> mat)
-      : centerLocations(center, vect(0, 0, 0)), radius(max(0.0, radius)),
-        mat(mat) {
-    vect rVec = vect(1, 1, 1) * radius;
-    bbox = aabb(center - rVec, center + rVec);
-  }
+  sphere(const point3 &center, double radius, shared_ptr<material> mat);
 
   sphere(const point3 &center1, const point3 &center2, double radius,
-         shared_ptr<material> mat)
-      : centerLocations(center1, center2 - center1), radius(max(0.0, radius)),
-        mat(mat) {
-    vect rVec = vect(1, 1, 1) * radius;
-    aabb box1 = aabb(center1 - rVec, center1 + rVec);
-    aabb box2 = aabb(center2 - rVec, center2 + rVec);
-    bbox = aabb(box1, box2);
-  }
+         shared_ptr<material> mat);
 
   virtual bool isHit(const ray &r, interval ray_t,
                      hitRecord &record) const override;
 
   virtual aabb boundingBox() const override { return bbox; };
+
+  void getUV(const point3 &p, hitRecord &record) const;
 };
 #endif // !SPHERE_H
